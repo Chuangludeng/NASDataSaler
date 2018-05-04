10117d3fad727d2181dcce46026a1cfb7007aad5
winsock <393441320@qq.com>
2018-05-04 23:20:32 +0800

    main

---
+++ src/main/java/com.littledemo/MainWindow.java
@@ -0,0 +1,29 @@
+package com.littledemo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import javax.xml.stream.Location;
+import java.io.IOException;
+import java.net.URL;
+
+public class MainWindow extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        URL url = getClass().getResource("/com.littledemo/MainWindow.fxml");
+
+        Parent root = FXMLLoader.load(url);
+
+        primaryStage.setTitle("My Application");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
