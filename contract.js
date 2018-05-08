2cb519ede3fb22e2cc13f6d1a23709ca3f40aff0
winsock <chuangludeng@gmail.com>
2018-05-08 05:37:49 +0000

    update contract

--- contract.js
+++ contract.js
@@ -36,60 +36,81 @@ BankVaultContract.prototype = {
     //TODO:
   },
 
-  save: function (height) {
+  createInfo: function (text,price) {
     var from = Blockchain.transaction.from;
-    var value = Blockchain.transaction.value;
-    var bk_height = new BigNumber(Blockchain.block.height);
+    var priceBigNum = new BigNumber(price);
+    var balance = new BigNumber(0);
 
     var orig_deposit = this.bankVault.get(from);
     if (orig_deposit) {
-      value = value.plus(orig_deposit.balance);
+      balance = balance.plus(orig_deposit.balance);
     }
 
     var deposit = new DepositeContent();
-    deposit.balance = value;
-    deposit.expiryHeight = bk_height.plus(height);
+    deposit.price = priceBigNum;
+    deposit.text = text;
+    deposit.balance = balance;
 
     this.bankVault.put(from, deposit);
   },
 
-  takeout: function (value) {
-    var from = Blockchain.transaction.from;
-    var bk_height = new BigNumber(Blockchain.block.height);
-    var amount = new BigNumber(value);
+  readInfo: function (address) {
 
-    var deposit = this.bankVault.get(from);
+    var deposit = this.bankVault.get(address);
     if (!deposit) {
-      throw new Error("No deposit before.");
+      throw new Error("No Info before.");
     }
 
-    if (bk_height.lt(deposit.expiryHeight)) {
-      throw new Error("Can not takeout before expiryHeight.");
+    if (Blockchain.transaction.value.lt(deposit.price)) {
+      throw new Error("Amount less than price.");
     }
+    Event.Trigger("readInfo", {
+          Transfer: {
+            from: Blockchain.transaction.from,
+            value: Blockchain.transaction.value
+          }
+        });
 
-    if (amount.gt(deposit.balance)) {
-      throw new Error("Insufficient balance.");
-    }
-
-    var result = Blockchain.transfer(from, amount);
-    if (!result) {
-      throw new Error("transfer failed.");
-    }
-    Event.Trigger("BankVault", {
-      Transfer: {
-        from: Blockchain.transaction.to,
-        to: from,
-        value: amount.toString()
-      }
-    });
-
-    deposit.balance = deposit.balance.sub(amount);
-    this.bankVault.put(from, deposit);
+    deposit.balance = deposit.balance.add(Blockchain.transaction.value);
+    this.bankVault.put(address, deposit);
+    
+    return deposit.text;
   },
+  
+  takeout: function (value) {
+  var from = Blockchain.transaction.from;
+  var amount = new BigNumber(value);
+
+  var deposit = this.bankVault.get(from);
+  if (!deposit) {
+    throw new Error("No deposit before.");
+  }
+
+  if (amount.gt(deposit.balance)) {
+    throw new Error("Insufficient balance.");
+  }
+
+  var result = Blockchain.transfer(from, amount);
+  if (!result) {
+    throw new Error("transfer failed.");
+  }
+  Event.Trigger("BankVault", {
+    Transfer: {
+      from: Blockchain.transaction.to,
+      to: from,
+      value: amount.toString()
+    }
+  });
+
+  deposit.balance = deposit.balance.sub(amount);
+  this.bankVault.put(from, deposit);
+},
+  
   balanceOf: function () {
     var from = Blockchain.transaction.from;
     return this.bankVault.get(from);
   },
+  
   verifyAddress: function (address) {
     // 1-valid, 0-invalid
     var result = Blockchain.verifyAddress(address);
